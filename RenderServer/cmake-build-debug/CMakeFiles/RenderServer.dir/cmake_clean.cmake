15dbeafa8ad751cf64914a32db6a1b80efe752c6
Ryan Collins <ryan@ohmgeek.co.uk>
2017-06-28 18:11:45 +0100

    Added C++ project for RenderServer

---
+++ RenderServer/cmake-build-debug/CMakeFiles/RenderServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RenderServer.dir/src/main.cpp.o"
+  "RenderServer.pdb"
+  "RenderServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RenderServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
