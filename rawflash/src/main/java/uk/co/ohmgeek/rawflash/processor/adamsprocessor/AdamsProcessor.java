95bef1769003ce5723f72110c2ad68682d1eccee
Ryan <ryan@ohmgeek.co.uk>
2017-10-16 11:44:04 +0100

    Create the Adams Processor

---
+++ rawflash/src/main/java/uk/co/ohmgeek/rawflash/processor/adamsprocessor/AdamsProcessor.java
@@ -0,0 +1,14 @@
+package uk.co.ohmgeek.rawflash.processor.adamsprocessor;
+
+import uk.co.ohmgeek.rawflash.processor.AbstractProcessor;
+
+import java.util.HashMap;
+
+public class AdamsProcessor implements AbstractProcessor {
+    // Adams Processor, named after Ansel Adams, a photographer and photo editor.
+
+    @Override
+    public void process(HashMap<String, String> operations) {
+
+    }
+}
