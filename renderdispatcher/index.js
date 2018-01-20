9a8f00b82d76d1fcc45f7ed7ae3ecc5a97e0a296
Ryan <ryan@ohmgeek.co.uk>
2018-01-20 15:37:18 +0000

    Connections all work between systems now

--- renderdispatcher/index.js
+++ renderdispatcher/index.js
@@ -1,41 +1,41 @@
 var amqp = require('amqplib/callback_api');
 var io = require('socket.io')(4000);
-
-
-
 var args = process.argv.slice(2);
-
-if (args.length == 0) {
-  console.log("Usage: rpc_client.js num");
-  process.exit(1);
-}
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
     ch.assertQueue('', {exclusive: true}, function(err, q) {
       var corr = generateUuid();
       // on a connection.
       io.on("connection", function(client) {
-        client.on("request-render", function(data) {
+        console.log("Connection made");
+        client.on("process-image", function(data) {
+          console.log("Request Render");
           // first create a request, by adding json info
           var newJSON = JSON.parse(data);
           newJSON['client'] = client.id;
-          ch.sendToQueue('rpc_queue',
+          console.log(newJSON);
+          ch.sendToQueue('rawflash',
             new Buffer(JSON.stringify(newJSON)),
             { correlationId: corr, replyTo: q.queue });
         });
-      })
 
-      ch.consume(q.queue, function(msg) {
-        if (msg.properties.correlationId == corr) {
-          var response = msg.content.toString();
-          var jsonStr = JSON.parse(response);
-          var clientid = jsonStr['client']; // todo get client id from JSON
-          var client = io.to(clientid);
-          // we need to convert to JSON.
-          client.emit("rendered", response);
-          setTimeout(function() { conn.close(); process.exit(0) }, 500);
-        }
-      }, {noAck: false});
+        ch.consume(q.queue, function(msg) {
+          if (msg.properties.correlationId == corr) {
+            console.log("REturn");
+            var response = msg.content.toString();
+            var jsonStr = JSON.parse(response);
+            var clientid = jsonStr['client']; // todo get client id from JSON
+            var client = io.to(clientid);
+            console.log(response);
+            // we need to convert to JSON.
+            client.emit("image-processed", response);
+            setTimeout(function() { conn.close(); process.exit(0) }, 500);
+          }
+        }, {noAck: false});
+
+      });
+
+
 
 
     });
