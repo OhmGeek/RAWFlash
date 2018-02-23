ad8dd667ff83cb2d75d0836b778e54e39f315d91
Ryan Collins <ryan@ohmgeek.co.uk>
2018-02-23 11:41:08 +0000

    Code refactoring for image picker

--- webservices/client/client/static/client/app.js
+++ webservices/client/client/static/client/app.js
@@ -2,6 +2,7 @@ var ImagePicker = {}
 
 
 $(function() {
+    // Deal with CSRF for Django not to throw errors
     function csrfSafeMethod(method) {
         // these HTTP methods do not require CSRF protection
         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
@@ -46,52 +47,50 @@ $(function() {
 
         // then display the page by refreshing.
     }
-    $.getJSON(window.location.origin + "/ajax/get_albums_for_user", function(data) {
-        $('#album_list').empty();
-        data.albums.forEach(function(listItem) {
-            var templateString = `
-              <li class="w3-bar">
-                <span class="w3-bar-item w3-button w3-xlarge w3-right">&#8250;</span>
-                <img src="img_avatar2.png" class="w3-bar-item w3-circle" style="width:85px">
-                <div class="w3-bar-item">
-                  <span class="w3-large"> {{album_name}} </span><br>
-                  <span> {{album_description}} </span>
-                </div>
-              </li>
-        `;
 
-            var listItemDOM = $('<li>');
-            listItemDOM.addClass('w3-bar')
 
-            // Add SPAN
-            var child = $('<span>');
-            child.addClass('w3-bar-item w3-button w3-xlarge w3-right');
-            child.text("&#8250;")
-            listItemDOM.append(child);
+    window.ImagePicker.displayAlbums = function() {
+        $.getJSON(window.location.origin + "/ajax/get_albums_for_user", function(data) {
+            $('#album_list').empty();
+            data.albums.forEach(function(listItem) {
+                var listItemDOM = $('<li>');
+                listItemDOM.addClass('w3-bar')
 
-            child = $('<img>');
-            child.addClass("w3-bar-item w3-circle");
-            child.attr('src', 'https://cataas.com/cat?width=60')
-            listItemDOM.append(child);
+                // Add SPAN
+                var child = $('<span>');
+                child.addClass('w3-bar-item w3-button w3-xlarge w3-right');
+                child.text("&#8250;")
+                listItemDOM.append(child);
 
-            child = $('<div>');
-            child.addClass('w3-bar-item');
+                child = $('<img>');
+                child.addClass("w3-bar-item w3-circle");
+                child.attr('src', 'https://cataas.com/cat?width=60')
+                listItemDOM.append(child);
 
-            var subChild = $('<span>');
-            subChild.addClass("w3-large");
-            subChild.text(listItem.album_name);
-            child.append(subChild);
+                child = $('<div>');
+                child.addClass('w3-bar-item');
 
-            subChild = $('<br>');
-            child.append(subChild);
+                var subChild = $('<span>');
+                subChild.addClass("w3-large");
+                subChild.text(listItem.album_name);
+                child.append(subChild);
 
-            subChild = $('<span>');
-            subChild.text(listItem.album_description);
-            child.append(subChild);
+                subChild = $('<br>');
+                child.append(subChild);
 
-            listItemDOM.append(child);
+                subChild = $('<span>');
+                subChild.text(listItem.album_description);
+                child.append(subChild);
 
-            $('#album_list').append(listItemDOM);
-        });
-    })
+                listItemDOM.append(child);
+
+                $('#album_list').append(listItemDOM);
+            });
+        })
+
+    }
+
+    // On load, display albums first.
+    window.ImagePicker.displayAlbums();
+
 });
