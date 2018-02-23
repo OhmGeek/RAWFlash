b9ef2cf9fbca56d733e18772049539a70873a4d2
Ryan Collins <ryan@ohmgeek.co.uk>
2018-02-23 15:09:22 +0000

    Album picker rendered in JS now

--- webservices/client/client/static/client/app.js
+++ webservices/client/client/static/client/app.js
@@ -16,22 +16,19 @@ $(function() {
     });
 
     window.ImagePicker.UI = {}
+    window.ImagePicker.Album = {}
+    window.ImagePicker.Images = {}
     window.ImagePicker.UI.createTitleDOM = function(titleText, descriptionText, buttons) {
-        // <div class="w3-container w3-green">
-        //     <button class="w3-button w3-teal w3-xlarge w3-right" onclick="window.ImagePicker.addAlbum()">Add</button>
-        //     <h2>Albums</h2>
-        //     <p>Here are your albums. Click on one to view and edit images.</p>
-        // </div>
+
         let titleSection = $('<div>');
         titleSection.addClass('w3-container w3-green');
 
-        buttons.forEach(function(index, button) {
+        buttons.forEach(function(button) {
             let btnElement = $('<button>');
-
+            console.log(button.text)
             btnElement.addClass('w3-button w3-teal w3-xlarge w3-right');
-            btnElement.text(buton.text);
             btnElement.on('click', button.event);
-
+            btnElement.text(button.text)
             titleSection.append(btnElement);
         });
 
@@ -48,7 +45,12 @@ $(function() {
     }
 
 
-    window.ImagePicker.addAlbum = function() {
+    window.ImagePicker.UI.displayPage = function(titleElement, bodyElement) {
+        $('#page-container').append(titleElement);
+        $('#page-container').append(bodyElement);
+    }
+
+    window.ImagePicker.Album.addAlbum = function() {
         // First, ask user to input album name
         swal({
                 title: 'New Album Name',
@@ -72,7 +74,7 @@ $(function() {
                 console.log(name);
                 return $.post(window.location.origin + "/ajax/add_album_for_user", { 'name': name, 'description': desc });
             }).then(function(resp) {
-                window.ImagePicker.displayAlbums(); // update album window
+                window.ImagePicker.Album.displayAlbums(); // update album window
             }).catch(function(err) {
                 swal("Error Adding Album", ":(", "error");
             })
@@ -82,7 +84,7 @@ $(function() {
     }
 
 
-    window.ImagePicker.displayAlbums = function() {
+    window.ImagePicker.Album.displayAlbums = function() {
         $.getJSON(window.location.origin + "/ajax/get_albums_for_user", function(data) {
             $('#album_list').empty();
             data.albums.forEach(function(listItem) {
@@ -123,7 +125,47 @@ $(function() {
 
     }
 
+
+    window.ImagePicker.Album.render = function() {
+        let buttons = []
+        buttons.push({
+            text: "Add",
+            event: function() {
+                window.ImagePicker.Album.addAlbum();
+            }
+        })
+
+        let listPage = $('<ul>');
+        listPage.addClass('w3-ul w3-hoverable');
+        listPage.attr('id', 'album_list');
+
+
+        let title = window.ImagePicker.UI.createTitleDOM("View Albums", "Here you can view the folders that images are sorted into", buttons)
+        window.ImagePicker.UI.displayPage(title, listPage)
+        window.ImagePicker.Album.displayAlbums();
+    }
+
+
+
+    window.ImagePicker.Images.render = function(album) {
+        let buttons = []
+        buttons.push({
+            text: "Upload",
+            event: function() {
+
+            }
+        });
+
+        let listPage = $('<ul>');
+        listPage.addClass('w3-ul w3-hoverable');
+        listPage.attr('id', 'image_list');
+
+
+        let title = window.ImagePicker.UI.createTitleDOM(album.name, album.description, buttons)
+        window.ImagePicker.UI.displayPage(title, listPage)
+    }
+
     // On load, display albums first.
-    window.ImagePicker.displayAlbums();
+    window.ImagePicker.Album.render()
 
 });
