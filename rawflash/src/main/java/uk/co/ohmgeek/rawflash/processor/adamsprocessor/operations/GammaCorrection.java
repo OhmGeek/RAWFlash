ccb3d0c8f0c6bceb0658c12c61b7562981c24ddd
Ryan Collins <ryan@ohmgeek.co.uk>
2018-02-26 10:52:33 +0000

    Update gamma correction code to not use JAva built in

--- rawflash/src/main/java/uk/co/ohmgeek/rawflash/processor/adamsprocessor/operations/GammaCorrection.java
+++ rawflash/src/main/java/uk/co/ohmgeek/rawflash/processor/adamsprocessor/operations/GammaCorrection.java
@@ -9,17 +9,30 @@ import java.awt.image.LookupOp;
 import java.awt.image.LookupTable;
 
 public class GammaCorrection implements IOperation {
-    private double gamma;
+    private int[] lookup;
     public GammaCorrection(double gamma) {
-        this.gamma = gamma;
+        // make gamma lookup array.
+        lookup = new int[256];
+        for(int j = 0; j < lookup.length; j++) {
+            lookup[j] = (int) (255 * Math.pow((double) j / (double) 255, gamma));
+        }
     }
     @Override
     public BufferedImage process(BufferedImage image) {
         // goes through lookup table,
         // looks up all values based on gamma
         // then adds that to the image.
-        LookupTable table = new GammaLookup(image.getWidth(), image.getHeight(), gamma);
-        LookupOp gammaCorrectionOp = new LookupOp(table, null);
-        return gammaCorrectionOp.filter(image, null);
+        for(int i = 0; i < image.getWidth(); i++) {
+            for(int j = 0; j < image.getHeight(); j++) {
+                int rgb = image.getRGB(i, j);
+                Color colour = new Color(rgb);
+                int newRed = lookup[colour.getRed()];
+                int newGreen = lookup[colour.getGreen()];
+                int newBlue = lookup[colour.getBlue()];
+
+                image.setRGB(i, j, new Color(newRed, newGreen, newBlue).getRGB());
+            }
+        }
+        return image;
     }
 }
