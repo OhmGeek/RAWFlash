9ea1bb1182e4a0d34c72070626b42329c1f9803a
Ryan Collins <ryan@ohmgeek.co.uk>
2018-04-12 16:07:05 +0100

    Contrast STretching done, but doesn't look right

--- rawflash/src/main/java/uk/co/ohmgeek/rawflash/processor/adamsprocessor/operations/UnsharpFilter.java
+++ rawflash/src/main/java/uk/co/ohmgeek/rawflash/processor/adamsprocessor/operations/UnsharpFilter.java
@@ -33,30 +33,52 @@ public class UnsharpFilter implements IOperation {
 
         // We need to create an image, with (image - smoothedImage)*amount + image result
         
+        float[][][] sharpenedImage = new float[image.getWidth()][image.getHeight()][3];
+        
+        // Minimum values to be used in the normalization
+        float minRed = Float.MAX_VALUE;
+        float minGreen = Float.MAX_VALUE;
+        float minBlue = Float.MAX_VALUE;
 
+        // Maximum Values to be used in the normalization
+        // TODO explain normalization
+        float maxRed = Float.MIN_VALUE;
+        float maxGreen = Float.MIN_VALUE;
+        float maxBlue = Float.MIN_VALUE;
         for (int i = 0; i < image.getWidth(); i++) {
             for (int j = 0; j < image.getHeight(); j++) {
                 Color pixel = new Color(image.getRGB(i, j));
                 Color smoothedPixel = new Color(smoothedImage.getRGB(i, j));
 
-                int newRed = Math.round((pixel.getRed() - smoothedPixel.getRed())*amount + pixel.getRed());
-                int newGreen = Math.round((pixel.getGreen() - smoothedPixel.getGreen())*amount + pixel.getGreen());
-                int newBlue =  Math.round((pixel.getBlue() - smoothedPixel.getBlue())*amount + pixel.getBlue());
+                float newRed = ((pixel.getRed() - smoothedPixel.getRed())*amount + pixel.getRed());
+                float newGreen = ((pixel.getGreen() - smoothedPixel.getGreen())*amount + pixel.getGreen());
+                float newBlue =  ((pixel.getBlue() - smoothedPixel.getBlue())*amount + pixel.getBlue());
 
-                // CLip at 255 if larger than needed.
-                newRed = newRed > 255 ? 255 : newRed;
-                newGreen = newGreen > 255 ? 255 : newGreen;
-                newBlue = newBlue > 255 ? 255 : newBlue;
+                minRed = newRed < minRed ? newRed : minRed; 
+                minGreen = newGreen < minGreen ? newGreen : minGreen; 
+                minBlue = newBlue < minBlue ? newBlue : minBlue; 
 
-                // Clip at 0 if smaller than needed.
-                newRed = newRed < 0 ? 0 : newRed;
-                newGreen = newGreen < 0 ? 0 : newGreen;
-                newBlue = newBlue < 0 ? 0 : newBlue;
+                maxRed = newRed > maxRed ? newRed : maxRed;
+                maxGreen = newGreen > maxGreen ? newGreen : maxGreen;
+                maxBlue = newBlue > maxBlue ? newBlue : maxBlue;
+                // store in an array, for normalization.
+                sharpenedImage[i][j][0] = newRed;
+                sharpenedImage[i][j][1] = newGreen;
+                sharpenedImage[i][j][2] = newBlue;
 
-                image.setRGB(i, j, new Color(newRed, newGreen, newBlue).getRGB());
             }
         }
-        // return new AddImageOp(edgeImage, amount).process(image);
+
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                // Constrast STretch (Normalize)
+                int red = Math.round(((sharpenedImage[i][j][0] - minRed)/(maxRed - minRed)) * 255);
+                int green = Math.round(((sharpenedImage[i][j][1] - minGreen)/(maxGreen - minGreen)) * 255);
+                int blue = Math.round(((sharpenedImage[i][j][2] - minBlue)/(maxBlue - minBlue)) * 255);
+
+                image.setRGB(i, j, (new Color(red, green, blue).getRGB()));
+            }
+        }
         return image;
     }
 }
